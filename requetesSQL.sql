83f4a6523698abd1339fbc8b499faca9720ae159
Fil2fer-vitamine <106022045+Fil2fer-vitamine@users.noreply.github.com>
2022-10-24 23:54:59 +0200

    Version 2
    Correction de questions que je pensais être bonnes - La réponse ne coïncidait pas vraiment avec ce qui était attendu pour certaines.
    Résolution de la question 30.

--- requetesSQL.sql
+++ requetesSQL.sql
@@ -42,9 +42,12 @@ where num_potion = 1 or num_potion = 3 or num_potion = 4;	-- Où "num_potion" es
 
 
 --7. Liste des trophées : numéro, date de prise, nom de la catégorie et nom du preneur. (10lignes)
-select t.num_trophee , t.date_prise , t.num_preneur  from trophee t ;
+select t.num_trophee , t.date_prise , c.nom_categ , h.nom  from trophee t
+join categorie c on c.code_cat = t.code_cat
+join habitant h on h.num_hab = t.num_preneur;
 -- Il nous faut une Sélection de "t.num_trophee", "t.date_prise" et "t.num_preneur" de la table 'trophee'.
 -- OK
+-- Correction au niveau du nom preneur.
 
 
 
@@ -68,12 +71,13 @@ where c.nom_categ = 'Bouclier de Légat';  -- recherche de lignes par rapport à
 
 
 --10. Liste des potions (libellés) fabriquées par Panoramix : libellé, formule et constituantprincipal. (3 lignes)
-select lib_potion 
+select lib_potion , p.formule , p.constituant_principal 
 from potion p -- Il nous faut une Sélection de "lib_potion" de la table 'potion' distante de 'fabriquer' et ensuite de 'habitant' où "h.nom" se trouve.
 join fabriquer f on f.num_potion = p.num_potion  -- ceci est un jointure entre les tables 'fabriquer' et 'potion'
 join habitant h on h.num_hab = f.num_hab  -- ceci est un autre jointure entre les tables 'habitant' et 'fabriquer'
 where h.nom = 'Panoramix';	-- recherche de lignes par rapport à une valeur unique --> l'opérateur logique '=' est à prendre en compte.
 -- OK
+-- Correction en ajoutant la formule et constituant principal.
 
 
 
@@ -119,15 +123,17 @@ where h.num_qualite is null ;  -- Où 'h.num_qualite' est inexistant : utilisati
 
 
 --15. Nom des habitants ayant consommé la Potion magique n°1 (c'est le libellé de lapotion) en février 52. (3 lignes)
-select h.nom, a.date_a  
+select h.nom    -- , a.date_a  
 from habitant h	-- Il nous faut une Sélection de "h.nom" et "a.date_a" - Seule la table 'habitant' est en référence.
 join absorber a on a.num_hab = h.num_hab	 -- ceci est une jointure entre les tables 'absorber' et 'habitant'
 join potion p on p.num_potion = a.num_potion	-- ceci est une jointure entre les tables 'potion' et 'absorber'
 where p.lib_potion = 'Potion magique n°1' 		-- recherche de lignes par rapport à une valeur unique --> l'opérateur logique '=' est à prendre en compte.	
-and a.date_a between '2052-02-01' and '2052-02-28';		-- dans un intervale de temps.
+and a.date_a between '2052-02-01' and '2052-02-29';		-- dans un intervale de temps.
 --between '2052-02-01' and '2052-02-28'; 
 --and a.date_a in (2052-02-01%); -- A revoir car il y a une erreur ... la notion du temps sur le mois de février, pouvant changer suivant si c'est une année bissextile, me perturbe ...
 -- OK
+-- Correction avec la date de 2052/02/29
+-- Mise en commentaire de l'affichage de la date pour coller à l'énoncé.
 
 
 
@@ -173,15 +179,15 @@ where h.nom = 'Goudurix'; 		-- recherche de lignes par rapport à une valeur uni
 
 
 --20. Date de première prise de trophée. (03/04/52)
-select min(date_prise) 
-from trophee t; -- J'étais resté sur cette solution, ...
--- Or, la solution trouvée ne répondait pas à la question : on recherche une date au formalisme française, ... et non américaine
+-- select min(date_prise) 
+-- from trophee t; 						-- J'étais resté sur cette solution, ...
+-- Or, la solution trouvée ne répondait pas à la question : on recherche une date au formalisme français, ... et non américain
 
 select to_char(min(t.date_prise), 'DD/MM/YY') 
 from trophee t;		-- Il nous faut une Sélection de "t.date_prise" dans le tableau 'trophee'
 -- pour avoir la valeur la plus basse, il y a utilisation de la méthode MIN().
 -- pour obtenir un certain formalisme, à savoir passer une date américaine à une date française,
--- utilisation de la méthode to_chart()
+-- --> utilisation de la méthode to_chart()
 -- OK
 
 
@@ -196,8 +202,8 @@ where p.lib_potion = ('Potion magique n°2');	 		-- Où il y a une valeur unique
 
 
 
---22. Superficie la plus grande. (895)
-select max(r.superficie) 
+--22. Superficie de la plus grande resserre. (895)
+select max(r.superficie)
 from resserre r;	-- Il nous faut une Sélection de "r.superficie" dans le tableau 'resserre'
 -- la méthode MAX() nous permet d'obtenir la valeur la plus haute.
 -- OK
@@ -207,19 +213,21 @@ from resserre r;	-- Il nous faut une Sélection de "r.superficie" dans le tablea
 --***
 
 --23. Nombre d'habitants par village (nom du village, nombre). (7 lignes)
-select distinct v.nom_village, count(h.nom)
+select v.nom_village, count(h.num_village)
 from habitant h	-- Il nous faut une Sélection de "v.nom_village" et "h.nom" - Seul le tableau 'absorber' est paramétré.
 join village v on v.num_village = h.num_village	-- ceci est une jointure entre les tables 'village' et 'habitant'
-group by v.nom_village, h.nom ;  -- pour l'affichage dès que la sélection est multiple.
+group by v.nom_village;  -- pour l'affichage dès que la sélection est multiple.
 -- Utilisation de la méthode count() pour compter les lignes.
 -- Utilisation du mot clé DISTINCT pour enlever les doublons.
 -- OK
+
+-- Attention : pas besoin de mettre le DISTINCT si nous n'avons pas de doublon ...
 
 
 
 
 --24. Nombre de trophées par habitant (6 lignes)
-select distinct  h.nom, count(t.num_trophee) 
+select h.nom, count(t.num_trophee) 
 from habitant h	-- Il nous faut une Sélection de "h.nom" et "t.num_trophee" - Seul le tableau 'habitant' est paramétré.
 join trophee t on h.num_hab = t.num_preneur	-- ceci est une jointure entre les tables 'trophee' et 'habitant'
 group by h.nom;	  -- pour l'affichage dès que la sélection est multiple.
@@ -229,11 +237,13 @@ group by h.nom;	  -- pour l'affichage dès que la sélection est multiple.
 -- Si je sélectionne le nom des habitants, je me dois de le grouper aux comptes de trophee : GROUP BY nous le permet.
 -- OK
 
+-- Attention : pas besoin de mettre le DISTINCT si nous n'avons pas de doublon ...
+
 select h.nom, count (t.num_preneur) 
 from trophee t 		-- Il nous faut une Sélection de "h.nom" et "t.num_preneur" - Seul le tableau 'habitant' est paramétré.
 join habitant h on t.num_preneur = h.num_hab -- ceci est une jointure entre les tables 'trophee' et 'habitant'
 group by h.nom;	  -- pour l'affichage dès que la sélection est multiple.
--- Cette solution a été trouvée par Soélie
+-- Cette solution a été trouvée par Soélie (ceci fait ressortir le t.num_preneur).
 --> le 'num_preneur' a été pris en compte pour le comptage.
 
 
@@ -251,7 +261,13 @@ group by p.nom_province;	  -- pour l'affichage dès que la sélection est multip
 -- Il me faut suivre impérativement le chemin --> habitant - village - province --> L'ordre dans lequel on met les jointures est IMPORTANT.
 -- OK
 
+select p.nom_province, round(AVG(h.age),+2) 
+from habitant h -- Il nous faut une Sélection de "p.nom_province" et "h.age" - Seul le tableau 'province' est paramétré.
+join village v on v.num_village = h.num_village -- ceci est une jointure entre les tables 'village' et 'habitant'
+join province p on p.num_province = v.num_province -- ceci est une jointure entre les tables 'province' et 'village'
+group by p.nom_province;
 
+-- Cette solution parait meilleure car il y a deux chiffres après la virgule : utilisation de round().
 
 
 
@@ -279,7 +295,7 @@ where a.quantite > 2 and p.lib_potion = 'Potion Zen'; -- Où il y a une valeur u
 
 --***
 --28. Noms des villages dans lesquels on trouve une resserre (3 lignes)
-select * 
+select v.nom_village  
 from village v		-- Il nous faut une Sélection de TOUT dans le tableau 'village'
 join resserre r on r.num_village = v.num_village	-- ceci est une jointure entre la table 'resserre' et 'village'
 -- "where num_resserre <> 0;"  -- on s'est aperçu que si l'on ne met pas un where, on a le même affichage !
@@ -294,11 +310,12 @@ join resserre r on r.num_village = v.num_village	-- ceci est une jointure entre 
 select v.nom_village, v.nb_huttes 
 from village v			-- Il nous faut une Sélection de "v.nom_village" et de "v.nb_huttes" dans le tableau 'village'
 order by v.nb_huttes desc		-- order by permet le tri par ordre alphabétique
+FETCH FIRST 1 row ONLY;		-- limit 1 donnerait la même chose.
 -- DESC permet le tri alphabétique par ordre DESCENDANT.
 
 -- 7 lignes apparaissent, on ne veut que la première ???
 
-FETCH FIRST 1 row ONLY;	-- limit 1 donnerait la même chose.
+
 
 select v.nom_village  from village v 
 order by nb_huttes desc limit 1;
@@ -312,35 +329,71 @@ where v.nb_huttes = (select max(v2.nb_huttes)from village v2);
 
 
 --30. Noms des habitants ayant pris plus de trophées qu'Obélix (3 lignes).
-select distinct h.nom, sum(t.date_prise)
-from habitant h 	-- Il nous faut une Sélection de "h.nom" et de "t.date_prise" - Seul le tableau 'habitant' est paramétré.
-join trophee t on t.num_preneur  = h.num_hab	-- ceci est une jointure entre la table 'trophee' et 'habitant'
-group by h.nom;  	-- pour l'affichage dès que la sélection est multiple.
-having SUM(t.date_prise) > 40
+-- Essai 1 : Echec
+--select distinct h.nom, sum(t.date_prise)
+--from habitant h 	-- Il nous faut une Sélection de "h.nom" et de "t.date_prise" - Seul le tableau 'habitant' est paramétré.
+--join trophee t on t.num_preneur  = h.num_hab	-- ceci est une jointure entre la table 'trophee' et 'habitant'
+--group by h.nom;  	-- pour l'affichage dès que la sélection est multiple.
+--having SUM(t.date_prise) > 40
 
 
 
-
+-- Essai 2 : Echec
 --join (select t.date_prise, h2.num_hab  from trophee t where h2.nom  > 'Obélix') as yoohoooooo on t.num_preneur = h.num_hab 
-group by h.nom, t.date_prise ;  -- Prise de 'date_prise' en référence pour cet exercice, ... plutôt que 'num_preneur' ...
+--group by h.nom, t.date_prise ;  -- Prise de 'date_prise' en référence pour cet exercice, ... plutôt que 'num_preneur' ...
 -- Echec : référence invalide d'une entrée de la clause FROM pour la table « h » Indice : Il existe une entrée pour la table « h » mais elle ne peut pas être référencée de cette partie de la requête.
 --> Essai de joindre h2 ?!?
 -- Echec autre : Il existe une entrée pour la table « h2 » mais elle ne peut pas être référencée de cette partie de la requête.
 
+-- Essai 3 : Echec
 --order by  t.num_preneur > (select t2.num_preneur from trophee t2 where h.nom = 'Obélix' );
 --if t.num_trophee > ((select t2.num_trophee from trophee t2)
 --select h2.nom from habitant h2  where h2.nom = 'Obélix');
 
-select h.nom, t.date_prise from habitant h
-join trophee t on t.num_trophee = h.num_hab 
-group by (h.nom, (select t2.date_prise from trophee t2 where t2.date_prise > ));
+-- Essai 4 : Echec
+--select h.nom, t.date_prise from habitant h
+--join trophee t on t.num_trophee = h.num_hab 
+--group by (h.nom, (select t2.date_prise from trophee t2 where t2.date_prise > ));
 
-select h.nom, count (t.num_preneur) as nbtrophee from trophee t 
+
+-- Essai 5 : Echec
+-- select h.nom, count (t.num_preneur) as nbtrophee from trophee t 
+-- join habitant h on t.num_preneur = h.num_hab 
+-- where h.nom > 'Obélix'
+-- group by h.nom;
+
+
+select h.nom, count (t.num_preneur) 
+from trophee t 		-- Il nous faut une Sélection de "h.nom" et "t.num_preneur" - Seul le tableau 'habitant' est paramétré.
+join habitant h on t.num_preneur = h.num_hab -- ceci est une jointure entre les tables 'trophee' et 'habitant'
+group by h.nom;  	-- Nous obtenons les six personnages ayant des trophees, ... Obélix y compris. Il nous faut un opérateur logique '>' ...
+					-- Si nous écrivons groupe by h.nom = 'Obélix'; --> ERREUR:
+					-- --> la colonne « h.nom » doit apparaître dans la clause GROUP BY ou doit être utilisé dans une fonction d'agrégat
+					-- ... mais avec quel valeur représentant Obélix ?
+
+-- Essai 6 : Echec.
+					-- Il nous faut cette logique > à la même logique appliquer juste pour Obélix.
+
+
+
+
+-- REPONSE A LA QUESTION 30 en combinant la réponse 24 par le biais de deux select :
+-- Cette solution a été trouvée par Soélie (ceci fait ressortir le t.num_preneur).
+
+select h.nom, count (t.num_preneur) 		-- Quand il y a un opérateur sup ou inf, il faut impérativement le mot clé 'having'
+-- 'having' se place après 'groupe by' :  select --> from --> join --> groupe by --> having
+from trophee t 		
 join habitant h on t.num_preneur = h.num_hab 
-where h.nom = 'Obélix'
-group by h.nom;
-
-
+group by h.nom		-- il y a deux éléments dans le premier select : utilisation de 'groupe by'.
+having count (t.num_preneur) > (select count (t2.num_preneur)			-- Le having ne fait la différence qu'entre deux conteurs semblables. 
+-- La première sélection prend en compte tous les personnages ayant des trophees.
+-- La deuxième sélection prend en compte le nombre de trophées pour Obélix
+-- ... d'où le calcul induisant HAVING élément 1 > élément 2.
+-- Emploi de 2 pour les indicatifs du deuxième select car on ne réutilise pas les tables d'origine pour le calcul.
+								from trophee t2 		 
+								join habitant h2 on t2.num_preneur = h2.num_hab 
+								where h2.nom = 'Obélix');		-- il n'y a qu'un élément dans le deuxième select : utilisation de 'where'.
+							-- Indentation souhaitée pour la lisibilité.
 
 
 
